da17afdf24142d7547c37a7ba3f34f620054e215
Pranav Mujumdar <psmujumdar@gmail.com>
2020-04-07 00:04:51 -0400

    Car physics hooked headlights hooked

--- Assets/CameraController.cs
+++ Assets/CameraController.cs
@@ -4,15 +4,18 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
+    public GameObject player;
+
+    private Vector3 offset;
     // Start is called before the first frame update
     void Start()
     {
-        
+        offset = transform.position - player.transform.position;
     }
-
-    // Update is called once per frame
-    void Update()
+    // LateUpdate is called after all Update functions have been called. 
+    void LateUpdate()
     {
-        
+        transform.position = player.transform.position + offset;
+        //transform.position = new Vector3(Input.mousePosition.x, transform.position.y, Input.mousePosition.z);
     }
 }
--- Assets/CarController.cs
+++ Assets/CarController.cs
@@ -1,19 +1,67 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+[RequireComponent(typeof(Rigidbody))]
 public class CarController : MonoBehaviour
 {
+
+    public float throttle;
+    public float steer;
+    public float strengthCoefficient = 20000f;
+    public float brakingTorque = 10f;
+
+    public List<WheelCollider> throttleWheels;
+    public List<GameObject> steeringWheels;
+    public List<WheelCollider> brakingWheels;
+    public List<GameObject> wheelMeshes;
+
+    public float maxTurnDegree = 20f;
+    public float handBrake;
     public Rigidbody car_rb;
-    // Start is called before the first frame update
-    void Start()
+
+    public Transform CM;
+
+    private void Start()
     {
-        
+        car_rb = GetComponent<Rigidbody>();
+        car_rb.centerOfMass = CM.position;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        car_rb.AddForce(new Vector3(0,0,1f) * 10, ForceMode.Acceleration);
+        throttle = Input.GetAxis("Vertical");
+        steer = Input.GetAxis("Horizontal");
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            handBrake = 1;
+        }
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            handBrake = 0;
+        }
+    }
+
+    
+    void FixedUpdate()
+    {
+        //car_rb.AddForce(new Vector3(0,0,1f) * 10, ForceMode.Acceleration);
+        foreach (WheelCollider wheel in throttleWheels)
+        {
+            wheel.motorTorque = strengthCoefficient * Time.fixedDeltaTime * throttle;
+        }
+        foreach (GameObject wheel in steeringWheels)
+        {
+            wheel.GetComponent<WheelCollider>().steerAngle = maxTurnDegree * steer;
+            wheel.transform.localEulerAngles = new Vector3(0f, steer*maxTurnDegree, 0f);
+        }
+        foreach (WheelCollider wheel in brakingWheels)
+        {
+            wheel.brakeTorque = brakingTorque * handBrake;
+        }
+
+        foreach(GameObject mesh in wheelMeshes)
+        {
+            mesh.transform.Rotate(car_rb.velocity.magnitude * (transform.InverseTransformDirection(car_rb.velocity).z >=0 ? 1 : -1)/ (2 * Mathf.PI * 0.17f), 0f,0f);
+        }
     }
 }
